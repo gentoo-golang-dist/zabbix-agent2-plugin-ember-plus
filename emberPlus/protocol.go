ad1c6d3e0269b34d0350fadc892f16c0ecc65a73
Eriks Sneiders <eriks.sneiders@zabbix.com>
2024-02-26 16:25:52 +0200

    ...G...... [ZBXNEXT-8997] added ember plus library

---
+++ emberPlus/protocol.go
@@ -0,0 +1,33 @@
+package ember
+
+var _ Encoder = &DefaultCodec{}
+var _ Decoder = &DefaultCodec{}
+
+const (
+	BOF              = 0xfe
+	Slot             = 0x00
+	MessageType      = 0x0e
+	CommandType      = 0x00
+	Version          = 1
+	DTDType          = 1
+	AppBytes         = 2
+	MinorVersion     = 40
+	MajorVersion     = 2
+	EOF              = 0xff
+	CE               = 0xfd
+	FirstMultiPacket = 0x80
+	LastMultiPacket  = 0x40
+	XORCE            = 0x20
+	BOFNE            = 0xf8
+
+	S101LenTilGlow = 10
+	CheckSumLen    = 2
+)
+
+type Decoder interface {
+	Decode(message []byte) ([]uint8, error)
+}
+
+type Encoder interface {
+	Encode(message []byte, packetType uint8) []uint8
+}
