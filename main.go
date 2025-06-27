f8fb2024c04fd6b09de84bf25d2133a9a2a83a18
Natalja Romancaka <natalja.romancaka@zabbix.com>
2025-06-27 08:41:45 +0300

    .......... [ZBXNEXT-826] updated release version to 7.0.16

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ the license. There is NO WARRANTY, to the extent permitted by law.`
 const (
 	PLUGIN_VERSION_MAJOR = 7
 	PLUGIN_VERSION_MINOR = 0
-	PLUGIN_VERSION_PATCH = 15
+	PLUGIN_VERSION_PATCH = 16
 	PLUGIN_VERSION_RC    = ""
 )
 
